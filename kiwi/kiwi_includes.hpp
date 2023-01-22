e6bf73ea1d24ca5d14cfc3df2fd7a034d55ce436
dav.g <david.gunter@alumni.epfl.ch>
2023-01-23 00:11:34 +0100

    deleted unused duplicate of renamed file.

---
+++ kiwi/kiwi_includes.hpp
@@ -0,0 +1,20 @@
+#ifndef KIWI_INCLUDES_HPP
+#define KIWI_INCLUDES_HPP
+
+#ifndef GLEW_STATIC
+#define GLEW_STATIC
+#endif // GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <string>
+#include <vector>
+#include <functional>
+#include <thread>
+#include <atomic>
+#include <chrono>
+#include <iostream>
+#include <cmath>
+#include <cstdint>
+
+#endif // KIWI_INCLUDES_HPP
